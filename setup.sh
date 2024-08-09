2c6e7fcf147855d0e7959fa87ec2fffd639b10c1
SamiKaab <sami.kaab.alain@gmail.com>
2024-08-09 14:35:54 +1000

    Update setup script to use correct file paths and change execution permission

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 
 # create variable for ankleflex path
-export ANKLEFLEX_PATH=/home/AnkleFlex
-
 echo "Setting up the environment"
-python -m venv $ANKLEFLEX_PATH/venv || (echo "failed to create virtual environment" && exit 1)
-source $ANKLEFLEX_PATH/venv/bin/activate || (echo "failed to activate virtual environment" && exit 1)
+python -m venv /home/ankleflex/AnkleFlex/venv 
+source /home/ankleflex/AnkleFlex/venv/bin/activate 
 
 echo "Installing requirements"
-pip install -r $ANKLEFLEX_PATH/requirements.txt || (echo "failed to install requirements" && exit 1)
+pip install -r /home/ankleflex/AnkleFlex/Src/requirements.txt
 
 echo "Setting up the crontab command"
-(crontab -l 2>/dev/null; echo "@reboot /home/AnkleFlex/venv/bin/python /home/AnkleFlex/Src/main.py") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot /home/ankleflex/AnkleFlex/venv/bin/python /home/ankleflex/AnkleFlex/Src/main.py") | crontab -
 
 echo "Setting up the hotspot"
 sudo nmcli device wifi hotspot con-name AnkleFlexHotspot ssid AnkleFlex password starseng
